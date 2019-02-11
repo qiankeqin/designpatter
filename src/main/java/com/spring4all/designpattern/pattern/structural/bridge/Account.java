e543acfde82712066fc4d7f5bf75913234e80817
qiankeqin <qiankeqin@51xf.cn>
2019-02-11 09:44:09 +0800

    桥接模式

---
+++ src/main/java/com/spring4all/designpattern/pattern/structural/bridge/Account.java
@@ -0,0 +1,11 @@
+package com.spring4all.designpattern.pattern.structural.bridge;
+
+/**
+ * @author qiankeqin
+ * @Description: 账号
+ * @date 2019-02-09 01:02
+ */
+public interface Account {
+    Account openAccount();
+    void showAccountType();
+}
