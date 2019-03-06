87a55114fa2480278c28c7710c30312f86f4eee6
qiankeqin <qiankeqin@51xf.cn>
2019-03-06 20:51:31 +0800

    test

--- src/main/java/com/spring4all/designpattern/controller/LoginController.java
+++ src/main/java/com/spring4all/designpattern/controller/LoginController.java
@@ -1,9 +1,30 @@
 package com.spring4all.designpattern.controller;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author qiankeqin
  * @Description: DESCRIPTION
  * @date 2019-03-05 13:47
  */
+@RestController
+@RequestMapping("/home")
 public class LoginController {
+
+    @RequestMapping("/login")
+    public String login(){
+        System.out.println("login start");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.itest.info");
+
+        String title = driver.getTitle();
+        System.out.printf(title);
+
+        driver.close();
+        System.out.println("login end");
+        return "success";
+    }
 }
